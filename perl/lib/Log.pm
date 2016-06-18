56d576308c8536e6e46c290665e6335d12b9dccb
Akira Osada <osd.akira@gmail.com>
2016-06-18 16:16:27 +0900

    課題perl-1

--- perl/lib/Log.pm
+++ perl/lib/Log.pm
@@ -1,6 +1,7 @@
 package Log;
 use strict;
 use warnings;
+use Time::Piece;
 
 sub new {
     my ($class, %args) = @_;
@@ -8,18 +9,32 @@ sub new {
 }
 
 sub protocol {
+  my $self = shift;
+  $self->{req} =~ /HTTP.+$/p;
+  return ${^MATCH};
 }
 
 sub method {
+  my $self = shift;
+  $self->{req} =~ /^[A-Z]+/p;
+  return ${^MATCH};
 }
 
 sub path {
+  my $self = shift;
+  my @path = split(/ /, $self->{req});
+  return $path[1];
 }
 
 sub uri {
+  my $self = shift;
+  return 'http://' . $self->{host} . $self->path;
 }
 
 sub time {
+  my $self = shift;
+  my $t = Time::Piece::gmtime($self->{epoch});
+  return $t->datetime;
 }
 
 1;
