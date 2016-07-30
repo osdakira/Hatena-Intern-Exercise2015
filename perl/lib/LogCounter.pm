85f157711cb3e9c280b4c492643375d0607545a9
Akira Osada <osd.akira@gmail.com>
2016-07-30 17:58:10 +0900

    logcounter のテストが通った

--- perl/lib/LogCounter.pm
+++ perl/lib/LogCounter.pm
@@ -2,15 +2,32 @@ package LogCounter;
 use strict;
 use warnings;
 
+use Data::Dumper;
+use Pry;
+
 sub new {
     my ($class, $logs) = @_;
     return bless { logs => $logs }, $class;
 };
 
 sub group_by_user {
+  my ($self) = @_;
+  my %hash;
+  my @logs = map { @$_ } $self->{logs};
+  foreach my $log (@logs) {
+    my $user = $log->{user} || "guest";
+    unless ($hash{$user}) {
+      $hash{$user} = [];
+    }
+    push(@{$hash{$user}}, $log);
+  }
+  return \%hash;
 }
 
 sub count_error {
+  my ($self) = @_;
+  my $count = grep { $_->{status} =~ /^5/ } map { @$_ } $self->{logs};
+  return $count;
 }
 
 1;
